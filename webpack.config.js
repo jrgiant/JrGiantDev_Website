717e04a6b288d81d84e1146e76f4dcc2d5fe643f
Joshua Rose <jrgiant_git@outlook.com>
2017-10-25 12:28:24 -0400

    node changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,30 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const extractLess = new ExtractTextPlugin({
+    filename: "[name].[contenthash].css",
+    disable: process.env.NODE_ENV === "development"
+});
 module.exports = {
     entry: "./src/js/bundler.js",
     output: {
         path: __dirname,
-        filename: "dist/scripts/jrgiantdev.js"
+        filename: "dist/scripts/jrgiantdev.js",
+        chunkFilename: "[name]-[chunckhash].js"
     },
     module: {
         rules: [
-            { test: /\.less$/, exclude:/^(https?:)?\/\//, use: "less-loader" }
+            { test: /\.less$/,  use: extractLess.extract({
+                use: [{
+                    loader: "css-loader"
+                }, {
+                    loader: "less-loader"
+                }],
+                // use style-loader in development
+                fallback: "style-loader"
+            })
+            }
         ]
-    }
+    },
+    plugins: [
+        extractLess
+    ]
 };

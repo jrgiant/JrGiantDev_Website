fe7ec4fbd16a3b178f39e277875f477e010c800a
Joshua Rose <jrgiant_git@outlook.com>
2017-11-10 15:05:05 -0500

    published

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,44 @@
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const extractSass = new ExtractTextPlugin({
-    filename: "[name].[contenthash].css",
+    filename: "./dist/style/style.css",
     disable: process.env.NODE_ENV === "development"
 });
 module.exports = {
-    entry: "./src/js/bundler.js",
+    entry: "./src/scripts/bundler.js",
     output: {
         path: __dirname,
-        filename: "dist/scripts/jrgiantdev.js",
+        filename: "./dist/scripts/jrgiantdev.js",
         chunkFilename: "[name]-[chunckhash].js"
     },
     module: {
         rules: [
-            { test: /\.Sass$/,  use: extractSass.extract({
+            { test: /\.scss$/,  use: extractSass.extract({
                 use: [{
-                    loader: "css-loader"
+                    loader: "css-loader",
+                    options:{url:false}
                 }, {
                     loader: "sass-loader"
                 }],
                 // use style-loader in development
                 fallback: "style-loader"
             })
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react','env' ]
+                }
             }
         ]
     },
     plugins: [
-        extractSass
+        extractSass,
+        (function () {
+            this.plugin('done', function (stats) {
+                console.log(('\n[Compiled at ' + new Date().toTimeString() + ']') + ' .\n');
+            });
+        })
     ]
 };
